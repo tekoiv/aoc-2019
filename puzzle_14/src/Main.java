b8aa0d74aec83a09c98c3bfc217e6665dc29d9d3
Topi Koivunen <tekoiv@utu.fi>
2019-12-14 15:24:58 +0200

    New approach.

--- puzzle_14/src/Main.java
+++ puzzle_14/src/Main.java
@@ -6,16 +6,15 @@ public class Main {
     static int oreRequired = 0;
     static Map<String, Integer> ingredientStorage = new HashMap<>();
 
-    static int getOreRequired(Map<List<String>, String> recipes, List<String> key) {
-        key.forEach(ingredient -> {
+    static int getOreRequired(Map<List<String>, String> recipes, List<String> key, Map<String, Integer> manufactoredFromOre) {
+        /*key.forEach(ingredient -> {
             List<String> nextKey = new ArrayList<>(getKeyByValue(recipes, ingredient)).get(0);
             int requiredAmount = getAmount(ingredient);
             int providedAmount = getAmount(recipes.get(nextKey));
-            nextKey = multiplyIngredients(nextKey, requiredAmount, providedAmount);
+            List<String> nextMultiplied = new ArrayList<>(multiplyIngredients(nextKey, requiredAmount, providedAmount));
             String ingredientID = ingredient.substring(ingredient.indexOf(" ") + 1);
             if(nextKey.get(0).substring(nextKey.get(0).indexOf(" ") + 1).equals("ORE")) {
-                int oreAmount = getAmount(nextKey.get(0));
-                //is in map
+                int oreAmount = getAmount(nextMultiplied.get(0));
                 if(ingredientStorage.containsKey(ingredientID)) {
                     if(ingredientStorage.get(ingredientID) < requiredAmount) {
                         if(requiredAmount / providedAmount == 1) {
@@ -44,7 +43,8 @@ public class Main {
             else {
                 getOreRequired(recipes, nextKey);
             }
-        });
+        });*/
+
         return oreRequired;
     }
 
@@ -53,12 +53,13 @@ public class Main {
     }
 
     static List<String> multiplyIngredients(List<String> ingredients, int requiredAmount, int providedAmount) {
+        List<String> list = new ArrayList<>();
         ingredients.forEach(ingredient -> {
             String currentAmount = ingredient.substring(0, ingredient.indexOf(" "));
             String multipliedAmount = Integer.toString(Integer.parseInt(currentAmount)*(requiredAmount / providedAmount));
-            ingredients.set(ingredients.indexOf(ingredient), ingredient.replace(currentAmount, multipliedAmount));
+            list.add(multipliedAmount);
         });
-        return ingredients;
+        return list;
     }
 
     static <T, E> Set<T> getKeyByValue(Map<T, E> map, E value) {
@@ -80,15 +81,17 @@ public class Main {
                 "2 AB, 3 BC, 4 CA => 1 FUEL").split("\n"));
         recipes.forEach(recipe -> System.out.println(recipe));
         Map<List<String>, String> recipeMap = new HashMap<>();
+        Map<String, Integer> manufactoredFromOre = new HashMap<>();
         recipes.forEach(recipe -> {
             List<String> key = Arrays.asList((recipe.substring(0, recipe.indexOf("="))).split(", "));
             key.set(key.size() - 1, key.get(key.size() - 1).substring(0, key.get(key.size() - 1).length() - 1));
             String value = recipe.substring(recipe.indexOf(">") + 2);
             recipeMap.put(key, value);
+            if(key.get(0).contains("ORE")) manufactoredFromOre.put(value.substring(value.indexOf(" ") + 1), 0);
         });
         List<String> firstKey = new ArrayList<>(getKeyByValue(recipeMap, "1 FUEL")).get(0);
         System.out.println(firstKey);
-        System.out.println(getOreRequired(recipeMap, firstKey));
+        System.out.println(getOreRequired(recipeMap, firstKey, manufactoredFromOre));
         //System.out.println(getOreRequired(recipeMap, firstKey));
     }
 }

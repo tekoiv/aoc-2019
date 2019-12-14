f010f229cbfbfad5216232ca2377bda2ffd0d80f
Topi Koivunen <tekoiv@utu.fi>
2019-12-14 19:20:18 +0200

    Almost time for a 180.

--- puzzle_14/src/Main.java
+++ puzzle_14/src/Main.java
@@ -4,9 +4,35 @@ import java.util.stream.Collectors;
 public class Main {
 
     static int oreRequired = 0;
-    static Map<String, Integer> ingredientStorage = new HashMap<>();
+    static Map<String, Integer> requiredIngredients = new HashMap<>();
 
-    static int getOreRequired(Map<List<String>, String> recipes, List<String> key, Map<String, Integer> manufactoredFromOre) {
+    static int getOreRequired(Map<List<String>, String> recipes, List<String> key) {
+        //first is 2AB
+        key.forEach(ingredient -> {
+            List<String> nextKey = new ArrayList<>(getKeyByValue(recipes, ingredient)).get(0);
+            int requiredAmount = getAmount(ingredient);
+            //int providedAmount = getAmount(recipes.get(nextKey));
+            String currentIngredient = ingredient.substring(ingredient.indexOf(" ") + 1);
+
+            if(recipes.get(key).contains("FUEL")) {
+                requiredIngredients.put(currentIngredient, requiredAmount);
+                getOreRequired(recipes, nextKey);
+            } else {
+                String product = recipes.get(key).substring(recipes.get(key).indexOf(" ") + 1);
+                String ing = recipes.get(key);
+                if(requiredIngredients.containsKey(currentIngredient)){
+                    int originalValue = requiredIngredients.get(currentIngredient);
+                    requiredIngredients.put(currentIngredient, originalValue + (requiredIngredients.get(product)/getAmount(product))*getAmount(currentIngredient));
+                } else {
+                    requiredIngredients.put(currentIngredient, requiredIngredients.get(product)/getAmount(ing)*getAmount(ingredient));
+                }
+            }
+            if(nextKey.get(0).substring(nextKey.get(0).indexOf(" ") + 1).equals("ORE")) {
+                 String product = recipes.get(key).substring(recipes.get(0).indexOf(" ") + 1);
+                 int provided = getAmount(recipes.get(key));
+                 oreRequired += requiredIngredients.get(currentIngredient)/provided
+            } else getOreRequired(recipes, nextKey);
+        });
         /*key.forEach(ingredient -> {
             List<String> nextKey = new ArrayList<>(getKeyByValue(recipes, ingredient)).get(0);
             int requiredAmount = getAmount(ingredient);
@@ -66,7 +92,6 @@ public class Main {
         return map.entrySet()
                 .stream()
                 .filter(entry -> entry.getValue().toString().substring(entry.getValue().toString().indexOf(" ") + 1).equals(value.toString().substring(value.toString().indexOf(" ") + 1)))
-                //.filter(entry -> Objects.equals(entry.getValue(), value))
                 .map(Map.Entry::getKey)
                 .collect(Collectors.toSet());
     }
@@ -81,17 +106,15 @@ public class Main {
                 "2 AB, 3 BC, 4 CA => 1 FUEL").split("\n"));
         recipes.forEach(recipe -> System.out.println(recipe));
         Map<List<String>, String> recipeMap = new HashMap<>();
-        Map<String, Integer> manufactoredFromOre = new HashMap<>();
         recipes.forEach(recipe -> {
             List<String> key = Arrays.asList((recipe.substring(0, recipe.indexOf("="))).split(", "));
             key.set(key.size() - 1, key.get(key.size() - 1).substring(0, key.get(key.size() - 1).length() - 1));
             String value = recipe.substring(recipe.indexOf(">") + 2);
             recipeMap.put(key, value);
-            if(key.get(0).contains("ORE")) manufactoredFromOre.put(value.substring(value.indexOf(" ") + 1), 0);
         });
         List<String> firstKey = new ArrayList<>(getKeyByValue(recipeMap, "1 FUEL")).get(0);
         System.out.println(firstKey);
-        System.out.println(getOreRequired(recipeMap, firstKey, manufactoredFromOre));
+        getOreRequired(recipeMap, firstKey);
         //System.out.println(getOreRequired(recipeMap, firstKey));
     }
 }

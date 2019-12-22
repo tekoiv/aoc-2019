b2f4011dbfa1273a270a15b9257103412907e43f
Topi Koivunen <tekoiv@utu.fi>
2019-12-22 10:09:34 +0200

    Key.java + reading the file.

--- puzzle_18/src/Main.java
+++ puzzle_18/src/Main.java
@@ -1,5 +1,20 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-
+        List<StringBuilder> input = new ArrayList<>();
+        StringBuilder line;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("../inputs/input_18.txt"));
+            line = new StringBuilder(reader.readLine());
+            while(line != null) {
+                input.add(line);
+                line = new StringBuilder(reader.readLine());
+            }
+        } catch(IOException e) { System.out.println(e); }
     }
 }

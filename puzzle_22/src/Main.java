0b0bd2d81850792c6217f0437e3e3d448b2d14d6
Topi Koivunen <tekoiv@utu.fi>
2019-12-23 10:37:10 +0200

    Part 22 a) done.

--- puzzle_22/src/Main.java
+++ puzzle_22/src/Main.java
@@ -1,23 +1,54 @@
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
 
-    private final int DECK_SIZE = 10;
+    /*
+    ArrayLists are the best approach, at least in part 1.
+     */
+
+    private final int DECK_SIZE = 10007;
     private List<Integer> cards;
 
     public static void main(String[] args) {
         Main main = new Main();
-        main.cutCards(-4);
-        main.printStack();
+        main.solve1();
     }
 
     private Main() {
         initCards();
+    }
+
+    private void solve1() {
+        List<String> input = parseInput();
+        for(String s: input) {
+            if(s.contains("deal into new stack"))
+                dealIntoNewStack();
+            else if(s.contains("deal with increment"))
+                dealWithIncrementN(Integer.parseInt(s.substring(s.indexOf("t ") + 2)));
+            else if(s.contains("cut"))
+                cutCards(Integer.parseInt(s.substring(s.indexOf("t ") + 2)));
+            else System.out.println("Error");
+        }
+        System.out.println("22 a): " + cards.indexOf(2019));
+    }
+
+    private List<String> parseInput() {
+        List<String> input = new ArrayList<>();
+        String line;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("../inputs/input_22.txt"));
+            line = reader.readLine();
+            while(line != null) {
+                input.add(line);
+                line = reader.readLine();
+            }
+        } catch(IOException e) { e.printStackTrace(); }
+        return input;
     }
 
     private void cutCards(int N) {
@@ -33,6 +64,21 @@ public class Main {
         cards = Stream.of(tail, head)
                 .flatMap(Collection::stream)
                 .collect(Collectors.toList());
+    }
+
+    private void dealWithIncrementN(int N) {
+        //use a map to prevent iteration over two arrayLists
+        Map<Integer, Integer> newDeck = new HashMap<>();
+        int cardIndex = 0;
+        for(int i = 0;;i += N) {
+            if(newDeck.size() == cards.size()) break;
+            newDeck.put(i % cards.size(), cards.get(cardIndex));
+            cardIndex++;
+        }
+        cards.clear();
+        for(int i = 0; i < newDeck.size(); i++) {
+            cards.add(newDeck.get(i));
+        }
     }
 
     private void dealIntoNewStack() {
